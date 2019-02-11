0e1de2c68ba419496d004285bf7775931b36585c
Yogesh <yesh.jaiswal16@gmail.com>
2019-02-11 18:36:00 +0530

    Added view and template for detailed blog post as well as CSS for the site 2.

--- ecom/settings.py
+++ ecom/settings.py
@@ -116,6 +116,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/blog/static/'
 
 MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
